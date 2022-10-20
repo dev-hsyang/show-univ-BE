d4721b1ebb7427cdad834f6beeb28ffb0694d977
hs.yang <didgsutmd1@ajou.ac.kr>
2022-10-20 13:43:16 +0900

    Initial commit

---
+++ src/main/java/com/konai/hsyang/konatoybe/KonatoyBeApplication.java
@@ -0,0 +1,13 @@
+package com.konai.hsyang.konatoybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonatoyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KonatoyBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/konai/hsyang/konatoybe/KonatoyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konai.hsyang.konatoybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonatoyBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
