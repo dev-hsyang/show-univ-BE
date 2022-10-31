d015605c28109fe31aab2e852390de0ca3e9f0c0
hs.yang <didgsutmd1@ajou.ac.kr>
2022-10-31 10:38:48 +0900

    changed: user api - find by username

---
+++ src/main/java/com/konai/hsyang/konatoybe/loginWebApi/api/UserWebApi.java
@@ -0,0 +1,21 @@
+package com.konai.hsyang.konatoybe.loginWebApi.api;
+
+import com.konai.hsyang.konatoybe.loginWebApi.domain.User;
+import com.konai.hsyang.konatoybe.loginWebApi.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class UserWebApi {
+
+    private final UserService userService;
+
+    @GetMapping("/api/user/{username}")
+    public User findByUsername(@PathVariable String username){
+
+        return userService.findByUsername(username);
+    }
+}
